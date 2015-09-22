ecfd27381f63f2229acd75e8133faa7aefed34f3
Shaan Sharma <shaansharma@outlook.com>
2015-09-22 17:23:55 -0400

    passing data confirmed working, now need to add the codonTranslate function

--- Genetic Code/codonAnswerController.swift
+++ Genetic Code/codonAnswerController.swift
@@ -24,7 +24,7 @@ class codonAnswerController: UIViewController {
     }
     
     func codonTranslate (codonIn: String) -> String{
-        return "TESTING";
+        return codonIn;
     }
     
 
