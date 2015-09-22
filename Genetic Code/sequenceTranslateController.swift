d55f0a0db6fad62fbef2062f62b15d757a54ace5
Shaan Sharma <shaansharma@outlook.com>
2015-09-22 16:58:39 -0400

    sequence translate added

---
+++ Genetic Code/sequenceTranslateController.swift
@@ -0,0 +1,18 @@
+//
+//  sequenceTranslateController.swift
+//  Genetic Code
+//
+//  Created by Shaan Sharma on 2015-09-22.
+//  Copyright © 2015 Shaan Sharma. All rights reserved.
+//
+
+import UIKit
+
+class sequenceTranslateController: UIViewController {
+    
+    @IBOutlet weak var squenceTextView: UITextView!
+    @IBOutlet weak var translateButton: UIButton!
+    @IBOutlet weak var translatedTextView: UITextView!
+    
+
+}
