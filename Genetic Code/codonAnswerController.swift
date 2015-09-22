e0fc7a717bb289ad462d53d55d65b35852589aca
Shaan Sharma <shaansharma@outlook.com>
2015-09-22 17:57:16 -0400

    codonTranslate completed

--- Genetic Code/codonAnswerController.swift
+++ Genetic Code/codonAnswerController.swift
@@ -40,6 +40,51 @@ class codonAnswerController: UIViewController {
         else if (codonIn == "gtt") || (codonIn == "gta") || (codonIn == "gtg") {
             return "val";
         }
+        else if (codonIn == "tcu") || (codonIn == "tcc") || (codonIn == "tca") || (codonIn == "tcg") || (codonIn == "agt") || (codonIn == "agc"){
+            return "ser";
+        }
+        else if (codonIn == "cct") || (codonIn == "ccc") || (codonIn == "cca") || (codonIn == "ccg"){
+            return "pro";
+        }
+        else if (codonIn == "act") || (codonIn == "acc") || (codonIn == "aca") || (codonIn == "acg"){
+            return "thr";
+        }
+        else if (codonIn == "gct") || (codonIn == "gcc") || (codonIn == "gca") || (codonIn == "gcg"){
+            return "ala";
+        }
+        else if (codonIn == "tat") || (codonIn == "tac"){
+            return "tyr";
+        }
+        else if (codonIn == "tag") || (codonIn == "tga") || (codonIn == "taa"){
+            return "STOP";
+        }
+        else if (codonIn == "cat") || (codonIn == "cac"){
+            return "his";
+        }
+        else if (codonIn == "caa") || (codonIn == "cag"){
+            return "gln";
+        }
+        else if (codonIn == "aaa") || (codonIn == "aag"){
+            return "lys";
+        }
+        else if (codonIn == "gat") || (codonIn == "gac"){
+            return "asp";
+        }
+        else if (codonIn == "gaa") || (codonIn == "gag"){
+            return "glu";
+        }
+        else if (codonIn == "tgg"){
+            return "trp";
+        }
+        else if (codonIn == "cgt") || (codonIn == "cgc") || (codonIn == "cga") || (codonIn == "cgg") || (codonIn == "aga") || (codonIn == "agg"){
+            return "arg";
+        }
+        else if (codonIn == "gct") || (codonIn == "gcc") || (codonIn == "gcg"){
+            return "gly";
+        }
+        else {
+            return "invalid sequence";
+        }
         
         
         return codonIn;
